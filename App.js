a5d506c3cf40f91f0f4b6c70530f89f02fdf1760
Pedro Pessoa <pedro.pessoa@futurice.com>
2018-03-19 00:25:57 +0200

    Add navigation header

--- App.js
+++ App.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { TabNavigator } from 'react-navigation';
+import { TabNavigator, StackNavigator } from 'react-navigation';
 
 import AuthScreen from './src/app/Screens/AuthScreen';
 import WelcomeScreen from './src/app/Screens/WelcomeScreen';
+import MapScreen from './src/app/Screens/MapScreen';
+import DeckScreen from './src/app/Screens/DeckScreen';
+import ReviewScreen from './src/app/Screens/ReviewScreen';
+import SettingsScreen from './src/app/Screens/SettingsScreen';
 
 export default class App extends React.Component {
   render() {
 
     const MainNavigator = TabNavigator({
       welcome: { screen: WelcomeScreen },
-      auth: { screen: AuthScreen }
+      auth: { screen: AuthScreen },
+      main: {
+        screen: TabNavigator({
+          map: { screen: MapScreen },
+          deck: { screen: DeckScreen },
+          review: {
+            screen: StackNavigator({
+              review: { screen: ReviewScreen },
+              settings: { screen: SettingsScreen }
+            })
+          }
+        })
+      }
     });
 
     return (
--- src/app/Screens/ReviewScreen.js
+++ src/app/Screens/ReviewScreen.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 
 export default class ReviewScreen extends React.Component {
+
+    static navigationOptions = ({ navigation }) => ({
+        title: 'Review Jobs',
+        headerRight: <Button title="Settings" onPress={() => { navigation.navigate('settings') }} />
+    })
+
     render() {
         return (
             <View>
