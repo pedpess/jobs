8e7f7c5439600fd727b86e5ac685ce87c3b6be6a
Pedro Pessoa <pedro.pessoa@futurice.com>
2018-03-19 00:31:04 +0200

    Add style and fix android marginTop

--- src/app/Screens/ReviewScreen.js
+++ src/app/Screens/ReviewScreen.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
 import { Button } from 'react-native-elements';
 
 export default class ReviewScreen extends React.Component {
 
     static navigationOptions = ({ navigation }) => ({
         title: 'Review Jobs',
-        headerRight: <Button title="Settings" onPress={() => { navigation.navigate('settings') }} />
+        headerRight: (
+            <Button
+                title="Settings"
+                onPress={() => { navigation.navigate('settings') }}
+                backgroundColor="rgba(0,0,0,0)"
+                color="rgba(0,122,255,1)"
+            />
+        ),
+        style: {
+            marginTop: Platform.OS === 'android' ? 24 :  0
+        }
     })
 
     render() {
