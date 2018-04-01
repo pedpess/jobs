f83537f107384a0b96cb58ab721c50dc3abb71d1
Pedro Pessoa <pedro.pessoa@futurice.com>
2018-04-01 23:59:20 +0300

    Refactor func to es7

--- src/app/actions/auth_actions.js
+++ src/app/actions/auth_actions.js
@@ -4,16 +4,14 @@ import {
     FACEBOOK_LOGIN_SUCCESS
 } from './types';
 
-export const facebookLogin = () => {
-    return async function (dispatch) {
-        let token = await AsyncStorage.getItem('fb_token');
+export const facebookLogin = () => async dispatch => {
+    let token = await AsyncStorage.getItem('fb_token');
 
-        if (token) {
-            // Dispatch action to use the existing token
+    if (token) {
+        // Dispatch action to use the existing token
 
-        } else {
-            // Start FB login process to get a new token
-        }
+    } else {
+        // Start FB login process to get a new token
     }
 };
 
