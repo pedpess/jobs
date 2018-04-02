5dd6e81e9dc7c773ecc89e4fbd35711b0069effc
Pedro Pessoa <pedro.pessoa@futurice.com>
2018-04-02 23:30:27 +0300

    Add reagion piece of state

--- src/app/Screens/MapScreen.js
+++ src/app/Screens/MapScreen.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
 import { MapView } from 'expo';
 
 export default class MapScreen extends React.Component {
+
+    state = {
+        region: {
+            longitude: -122,
+            latitude: 37,
+            latitudeDelta: 0.04,
+            longitudeDelta: 0.09,
+        },
+        mapLoaded: false,
+    }
+
+    componentDidMount() {
+        this.setState({ mapLoaded: true });
+    }
+
     render() {
+
+        if (!this.state.mapLoaded) {
+            return (
+                <View style={{ flex: 1, justifyContent: 'center' }}>
+                    <ActivityIndicator size="large" />
+                </View>
+            );
+        }
         return (
             <View style={{ flex: 1 }}>
-                <MapView style={{ flex: 1 }} />
+                <MapView
+                    map
+                    region={this.state.region}
+                    style={{ flex: 1 }}
+                />
             </View>
         );
     }
