a34865338fe3e947d127be2db6902341c40a90e0
Pedro Pessoa <pedro.pessoa@futurice.com>
2018-04-06 21:26:32 +0300

    Retrieve geocode data

--- src/app/Screens/MapScreen.js
+++ src/app/Screens/MapScreen.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { View, Text, ActivityIndicator } from 'react-native';
 import { MapView } from 'expo';
+import { connect } from 'react-redux';
 
-export default class MapScreen extends React.Component {
+import * as actions from '../actions';
+import { Button } from 'react-native-elements';
+
+class MapScreen extends React.Component {
 
     state = {
         region: {
@@ -18,6 +22,14 @@ export default class MapScreen extends React.Component {
         this.setState({ mapLoaded: true });
     }
 
+    onRegionChangeComplete = (region)  => {
+        this.setState({ region });
+    }
+
+    onButtonPress = () => {
+        this.props.fetchJobs(this.state.region);
+    }
+
     render() {
 
         if (!this.state.mapLoaded) {
@@ -30,11 +42,31 @@ export default class MapScreen extends React.Component {
         return (
             <View style={{ flex: 1 }}>
                 <MapView
-                    map
                     region={this.state.region}
                     style={{ flex: 1 }}
+                    onRegionChangeComplete={this.onRegionChangeComplete}
                 />
+                <View style={styles.buttonContainer}>
+                    <Button
+                        large
+                        title="Search This Area"
+                        backgroundColor="#009688"
+                        icon={{ name: 'search' }}
+                        onPress={this.onButtonPress}
+                    />
+                </View>
             </View>
         );
     }
 }
+
+export default connect(null, actions)(MapScreen);
+
+const styles = {
+    buttonContainer: {
+        position: 'absolute',
+        bottom: 20,
+        left: 0,
+    }
+}
+
--- src/app/actions/types.js
+++ src/app/actions/types.js
@@ -1,2 +1,3 @@
 export const FACEBOOK_LOGIN_SUCCESS = 'facebook_login_success';
 export const FACEBOOK_LOGIN_FAILED = 'facebook_login_failed';
+export const FETCH_JOBS = 'fetch_jobs';
