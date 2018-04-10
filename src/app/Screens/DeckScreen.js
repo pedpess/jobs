e042519dff44e33132c03a5ac0ea5e6dbd417d50
Pedro Pessoa <pedro.pessoa@futurice.com>
2018-04-11 00:33:53 +0300

    Add Swipe component

--- src/app/Screens/DeckScreen.js
+++ src/app/Screens/DeckScreen.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import Swipe from '../components/Swipe';
 
-export default class DeckScreen extends React.Component {
+class DeckScreen extends React.Component {
     render() {
         return (
             <View>
-                <Text>DeckScreen</Text>
-                <Text>DeckScreen</Text>
-                <Text>DeckScreen</Text>
-                <Text>DeckScreen</Text>
-                <Text>DeckScreen</Text>
-                <Text>DeckScreen</Text>
+                <Swipe
+                    data={this.props.jobs}
+                />
             </View>
         );
     }
 }
+
+function mapStateToProps({ jobs }) {
+    return { jobs: jobs.results };
+}
+
+export default connect(mapStateToProps)(DeckScreen);
