6315ef2dd432eff6511a10ae6d7657a6acc4aecb
Pedro Pessoa <pedro.pessoa@futurice.com>
2018-04-15 22:48:17 +0300

    Render cards with location

--- src/app/Screens/AuthScreen.js
+++ src/app/Screens/AuthScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, AsyncStorage } from 'react-native';
+import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 
 import * as actions from '../actions';
--- src/app/Screens/DeckScreen.js
+++ src/app/Screens/DeckScreen.js
@@ -1,17 +1,68 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
 import { connect } from 'react-redux';
+import { MapView } from 'expo';
+import { Card, Button } from 'react-native-elements';
 import Swipe from '../components/Swipe';
 
 class DeckScreen extends React.Component {
+
+    renderCard(job) {
+
+        const initialRegion = {
+            longitude: job.longitude,
+            latitude: job.latitude,
+            latitudeDelta: 0.045,
+            longitudeDelta: 0.02
+        }
+
+        return (
+            <Card title={job.jobtitle}>
+                <View style={{ height: 300 }}>
+                    <MapView
+                        scrollEnabled={false}
+                        style={{ flex: 1 }}
+                        cacheEnabled={Platform.OS === 'android' ? true : false}
+                        initialRegion={initialRegion}
+                    >
+                    </MapView>
+                </View>
+                <View style={styles.detailsWrapper}>
+                    <Text>{job.company}</Text>
+                    <Text>{job.formattedRelativeTime}</Text>
+                </View>
+                <Text>
+                    {job.snippet.replace(/<b>/g, '').replace(/<\/b/g, '')}
+                </Text>
+            </Card>
+        );
+    }
+
+    renderNoMoreCards() {
+        return (
+            <Card title="No more jobs">
+            </Card>
+        );
+    }
+
     render() {
         return (
-            <View>
+            <View style={{ marginTop: 10 }}>
                 <Swipe
                     data={this.props.jobs}
+                    renderCard={this.renderCard}
+                    renderNoMoreCards={this.renderNoMoreCards}
+                    keyProp="jobkey"
                 />
             </View>
         );
+    }
+}
+
+const styles = {
+    detailsWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'space-around'
     }
 }
 
